f8662c91cae0ecc7b6ef874a525aa4d366083ca0
a1noack <adamnoack1@gmail.com>
2019-08-24 17:40:55 -0700

    make activation function of lenet model a constructor parameter

--- lenet.py
+++ lenet.py
@@ -4,8 +4,9 @@ import torch.nn.functional as F
 class LeNet(nn.Module):
     """MNIST-modified LeNet-5 model.
     """
-    def __init__(self):
+    def __init__(self, activation=F.relu):
         super(LeNet, self).__init__()
+        self.activation = activation
         self.conv1 = nn.Conv2d(1, 6, kernel_size=5, stride=1, padding=2)
         self.pool1 = nn.MaxPool2d(kernel_size=2)
         self.conv2 = nn.Conv2d(6, 16, kernel_size=5, stride=1, padding=0)
@@ -17,10 +18,10 @@ class LeNet(nn.Module):
         self.fc3 = nn.Linear(84,10)
 
     def forward(self, x):
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.pool1(self.activation(self.conv1(x)))
+        x = self.pool2(self.activation(self.conv2(x)))
         x = x.view(x.shape[0], -1)
-        x = self.fc1_drop(F.relu(self.fc1(x)))
-        x = self.fc2_drop(F.relu(self.fc2(x)))
+        x = self.fc1_drop(self.activation(self.fc1(x)))
+        x = self.fc2_drop(self.activation(self.fc2(x)))
         x = self.fc3(x)
         return F.log_softmax(x, dim=1)
