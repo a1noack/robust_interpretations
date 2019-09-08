ee13772483b9543174f697181d1ef129b5ff7942
a1noack <adamnoack1@gmail.com>
2019-09-08 13:35:24 -0700

    create new function to randomly perturb samples and rename attacking function

--- utils/adv_ex_utils.py
+++ utils/adv_ex_utils.py
@@ -1,7 +1,7 @@
 import torch
 from advertorch.attacks import GradientSignAttack, CarliniWagnerL2Attack, PGDAttack
 
-def generate_adversarial_samples(og_samples, true_labels, adversary, num_per_samp=1):
+def generate_adv_exs(og_samples, true_labels, adversary, num_per_samp=1):
     """Create num_per_samp adversarial examples for each sample in
     og_samples. Return the generated samples along with corresponding 
     adv_labels, a tensor containing the adversarial examples' labels.
@@ -12,3 +12,11 @@ def generate_adversarial_samples(og_samples, true_labels, adversary, num_per_sam
     adv_samples = torch.cat(adv_samples, 0)
     adv_labels = torch.cat([true_labels]*num_per_samp, 0)
     return adv_samples, adv_labels
+
+def perturb_randomly(og_samples, scale=1.):
+    """Add a random variable drawn from a normal distribution with 
+    specified parameters to each dimension of each sample and return.
+    """
+    normal = tdist.Normal(loc=torch.tensor([0.]), scale=torch.tensor([scale]))
+    shape = list(og_samples.shape)
+    return og_samples + normal.sample(shape).reshape(shape)
