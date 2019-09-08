fda02175166870721d83c0d7ab30e48b5629bd2c
a1noack <adamnoack1@gmail.com>
2019-09-08 14:43:57 -0700

    add parallelized but unnormalized version of simple gradients function

--- utils/interpretation_generators.py
+++ utils/interpretation_generators.py
@@ -35,3 +35,19 @@ def smoothgrad(net, sample, label, j=15, scale=1., used=True):
     saliency = saliency.mean(dim=0)
     saliency = saliency/torch.sum(saliency)
     return saliency
+
+def simple_gradients(net, samples, labels, used=True):
+    """Creates simple gradient saliency map.
+    """
+    if not used:
+        samples = torch.autograd.Variable(samples, requires_grad=True)
+    net(samples)
+    logits = net.logits
+    bp_mat = torch.nn.functional.one_hot(labels, num_classes=10).float()
+    grads = torch.autograd.grad(logits * bp_mat, samples, grad_outputs=bp_mat, create_graph=True)[0].squeeze()
+    # equivalent to
+#     logit = torch.sum(bp_mat * logits)
+#     grads = torch.autograd.grad(logit, sample)[0].squeeze()
+    saliency = torch.abs(grads)
+#     saliency = saliency/torch.sum(saliency)
+    return saliency
