029416c18db5a3e439eb03626be8d5d69d62b381
a1noack <adamnoack1@gmail.com>
2019-09-07 18:38:05 -0700

    separate interpretation generating code from attack code and add smoothgrad generator

--- utils/interpretation_generators.py
+++ utils/interpretation_generators.py
@@ -4,7 +4,8 @@ import torch.distributions as tdist
 def simple_gradient(net, sample, label, used=True):
     """Creates simple gradient saliency map.
     """
-    sample = torch.autograd.Variable(sample, requires_grad=True)
+    if not used:
+        sample = torch.autograd.Variable(sample, requires_grad=True)
     net(sample)
     logits = net.logits
     bp_mat = torch.nn.functional.one_hot(label, num_classes=10).float()
@@ -19,14 +20,16 @@ def simple_gradient(net, sample, label, used=True):
 def smoothgrad(net, sample, label, j=15, scale=1., used=True):
     """Creates smoothgrad saliency map.
     """
-    sample = torch.autograd.Variable(sample, requires_grad=True)
+    if not used:
+        sample = torch.autograd.Variable(sample, requires_grad=True)
     normal = tdist.Normal(loc=torch.tensor([0.]), scale=torch.tensor([scale]))
     shape = list(sample.shape)
     shape[0] = j
     samples = sample + normal.sample(shape).reshape(shape)
+    samples = torch.cat([samples, sample], dim=0)
     net(samples)
     logits = net.logits 
-    bp_mat = torch.nn.functional.one_hot(label.repeat(j,1), num_classes=10).float().squeeze()
+    bp_mat = torch.nn.functional.one_hot(label.repeat(j+1,1), num_classes=10).float().squeeze()
     grads = torch.autograd.grad(logits * bp_mat, samples, grad_outputs=bp_mat, create_graph=True)[0].squeeze()
     saliency = torch.abs(grads)
     saliency = saliency.mean(dim=0)
