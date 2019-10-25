1e4e8fb6ecdee62624990b7d682f3aadc6e7a3b6
a1noack <adamnoack1@gmail.com>
2019-10-24 21:22:12 -0700

    fix position of dropout to comply with tensorflow tutorial

--- utils/models.py
+++ utils/models.py
@@ -73,15 +73,15 @@ class SimpleCNN(nn.Module):
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2)
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.pool2_drop = nn.Dropout(p=p_drop)
         self.fc1 = nn.Linear(7*7*64, 1024)
+        self.fc1_drop = nn.Dropout(p=p_drop)
         self.fc2 = nn.Linear(1024, 10)
         
     def forward(self, x):
-        x = self.pool1(self.activation(self.conv1(x)))
+        x = self.pool1(self.activation(self.conv1(x)))        
         x = self.pool2(self.activation(self.conv2(x)))
-        x = self.pool2_drop(x.view(x.shape[0],-1))
-        x = self.fc1(x)
+        x = x.view(x.shape[0],-1)
+        x = self.fc1_drop(self.activation(self.fc1(x)))
         self.logits = self.fc2(x)
         self.probabilities = F.softmax(self.logits, dim=1)
         return self.logits
