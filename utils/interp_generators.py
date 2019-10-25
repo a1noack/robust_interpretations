1c6c868bf21ce74683e974ca599ba49a97635b42
a1noack <adamnoack1@gmail.com>
2019-10-24 21:18:28 -0700

    make absolute value conditional and max across channels
    -make the converting of salience maps to their absolute value
    counterparts optional
    -compute maximum across channels for rgb images instead of mean when
    creating a salience map. This is to comply with recommendations in the
    simple gradient paper.

--- utils/interp_generators.py
+++ utils/interp_generators.py
@@ -1,7 +1,7 @@
 import torch
 import torch.distributions as tdist
 
-def smoothgrad(net, sample, label, normalize=True, j=50, scale=1., used=True, for_loss=False):
+def smoothgrad(net, sample, label, normalize=True, abs_val=True, j=50, scale=1., used=True, for_loss=False):
     """Creates smoothgrad saliency map. Unparallelized.
     """
     if not used:
@@ -11,31 +11,34 @@ def smoothgrad(net, sample, label, normalize=True, j=50, scale=1., used=True, fo
     shape[0] = j
     samples = sample + normal.sample(shape).reshape(shape)
     samples = torch.cat([samples, sample], dim=0)
-    net(samples)
-    logits = net.logits 
+    logits = net(samples)
     bp_mat = torch.nn.functional.one_hot(label.repeat(j+1,1), num_classes=logits.shape[1]).float().squeeze()
-    grads = torch.autograd.grad(logits * bp_mat, samples, grad_outputs=bp_mat, create_graph=for_loss)[0].squeeze()
-    saliency = torch.abs(grads)
+    grads = torch.autograd.grad(logits, samples, grad_outputs=bp_mat, create_graph=for_loss)[0].squeeze()
+    if abs_val:
+        saliency = torch.abs(grads)
     saliency = saliency.mean(dim=0)
     if saliency.shape[0] == 3:
-        saliency = torch.mean(saliency, dim=0, keepdims=True)
+        saliency = torch.max(saliency, dim=0, keepdims=True)[0]
     if normalize:
         saliency = saliency/torch.sum(saliency)
+        
     return saliency
 
-def simple_gradient(net, samples, labels, normalize=True, used=True, for_loss=False):
+def simple_gradient(net, samples, labels, normalize=True, abs_val=True, used=True, for_loss=False):
     """Parallelized version of simple gradient saliency map function.
     """
     if not used:
         samples = torch.autograd.Variable(samples, requires_grad=True)
-    net(samples)
-    logits = net.logits
-    bp_mat = torch.nn.functional.one_hot(labels, num_classes=10).float()
-    grads = torch.autograd.grad(logits * bp_mat, samples, grad_outputs=bp_mat, create_graph=for_loss)[0]#.squeeze()
-    saliency = torch.abs(grads)
+    logits = net(samples)
+    bp_mask = torch.nn.functional.one_hot(labels, num_classes=10).float()
+#     label_logits = torch.sum(logits * bp_mask, dim=1, keepdim=True)
+    grads = torch.autograd.grad(logits, samples, grad_outputs=bp_mask, create_graph=for_loss)[0]
+    if abs_val:
+        saliency = torch.abs(grads)
     # this is needed to compress 3 channel map to 1 channel
+    # changed from mean to max
     if saliency.shape[1] == 3:
-        saliency = torch.mean(saliency, dim=1, keepdims=True)
+        saliency = torch.max(saliency, dim=1, keepdims=True)[0]
     # expected to have no channel dimension
     saliency = saliency.squeeze()
     # normalize each saliency map by dividing each component of each saliency map
