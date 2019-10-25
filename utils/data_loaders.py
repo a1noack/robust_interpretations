de6e22a1dab29f5b36c82f91f57f64ac501f7fea
a1noack <adamnoack1@gmail.com>
2019-10-24 21:15:43 -0700

    make various changes
    -make CIFAR-10 images' pixel values scaled between zero and one
    -add dataset for MNIST dataset with a validation set
    -add functionality for CIFAR-10 dataset with target interpretations

--- utils/data_loaders.py
+++ utils/data_loaders.py
@@ -1,6 +1,6 @@
 import torchvision
 import torch 
-from utils.target_interps_dataset import MNIST_Interps_Dataset
+from utils.target_interps_dataset import MNIST_Interps_Dataset, MNIST_val, CIFAR10_Interps_Dataset
 
 class DataLoader():
     def __init__(self, dataset, tr_batch_size=64, te_batch_size=50, augment=True, model='simplecnn', path='./data', thresh=None):
@@ -24,6 +24,28 @@ class DataLoader():
                                      transform=self.data_preprocess), 
                                 batch_size=te_batch_size, 
                                 shuffle=False)
+        elif dataset == 'MNIST_val':
+            self.tr_batch_size = tr_batch_size
+            self.te_batch_size = te_batch_size
+
+            # the mean of mnist pixel data is .1307 and the stddev is .3081
+            self.data_preprocess = torchvision.transforms.Compose([
+                                    torchvision.transforms.ToTensor()])
+#                                     torchvision.transforms.Normalize((0.1307,), (0.3081,))])
+            
+            self.train_loader = torch.utils.data.DataLoader(
+                                MNIST_val(root=f'{path}/MNIST/processed/', section='train', transform=self.data_preprocess), 
+                                batch_size=tr_batch_size, 
+                                shuffle=True)
+            self.val_loader = torch.utils.data.DataLoader(
+                                MNIST_val(root=f'{path}/MNIST/processed/', section='val', transform=self.data_preprocess), 
+                                batch_size=te_batch_size, 
+                                shuffle=True)
+
+            self.test_loader = torch.utils.data.DataLoader(
+                                MNIST_val(root=f'{path}/MNIST/processed/', section='test', transform=self.data_preprocess), 
+                                batch_size=te_batch_size, 
+                                shuffle=False)
         elif dataset == 'CIFAR-10':
             self.tr_batch_size = tr_batch_size
             self.te_batch_size = te_batch_size
@@ -31,7 +53,7 @@ class DataLoader():
             # Normalize the test set same as training set without augmentation
             self.test_preprocess = torchvision.transforms.Compose([
                 torchvision.transforms.ToTensor(),
-                torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+#                 torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
             ])
             
             if augment:
@@ -65,13 +87,24 @@ class DataLoader():
             
             self.train_data = MNIST_Interps_Dataset(
                 root=f'{path}/MNIST/{model}_mnist_interps/', train=True, thresh=thresh)
-            
             self.train_loader = torch.utils.data.DataLoader(
                 self.train_data, batch_size=tr_batch_size, shuffle=True)
             
             self.test_data = MNIST_Interps_Dataset(
                 root=f'{path}/MNIST/{model}_mnist_interps/', train=False, thresh=thresh)
+            self.test_loader = torch.utils.data.DataLoader(
+                self.test_data, batch_size=te_batch_size, shuffle=False)
+        elif dataset == 'CIFAR-10_interps':
+            self.tr_batch_size = tr_batch_size
+            self.te_batch_size = te_batch_size
             
+            self.train_data = CIFAR10_Interps_Dataset(
+                root=f'{path}/ddnet_cifar10_interps/', train=True, thresh=thresh)
+            self.train_loader = torch.utils.data.DataLoader(
+                self.train_data, batch_size=tr_batch_size, shuffle=True)
+            
+            self.test_data = MNIST_Interps_Dataset(
+                root=f'{path}/ddnet_cifar10_interps/', train=False, thresh=thresh)
             self.test_loader = torch.utils.data.DataLoader(
                 self.test_data, batch_size=te_batch_size, shuffle=False)
         else:
